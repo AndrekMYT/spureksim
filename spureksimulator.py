c6e21fb8e8ffc57e9953185e1f653a14404f9e29
AndrekMYT <82237604+AndrekMYT@users.noreply.github.com>
2022-03-28 17:19:04 +0200

    added Joe mama

--- spureksimulator.py
+++ spureksimulator.py
@@ -62,6 +62,56 @@ if Gamer == 'Korwin' or Gamer == 'JKM':
     else:
         print('Gays won :c')
         print(f'If not Korwin\'s death he would win the game with {pktg} points')
+elif Gamer == 'joe mama' or Gamer == 'ur mom' or Gamer == 'Joe mama':
+    dmgs = random.randint(1,20)
+    dmgp = random.randint(1,20)
+    while hpp > 0 and hps > 0:
+        if z1 >= z2 and choice == 'f':
+            print ('Joe mama attacks')
+            hps = hps - dmgp
+            z1 = 0
+            z2 = 1
+            pktg +=1
+            if hps >0:
+                print(f'Joe dada still have {hps} hp')
+
+        elif z1 >= z2 and choice == 'h':
+            if pktg > 0:
+                z1 = 0
+                z2 = 1
+                pktg -=1
+                hpp = hpp + dmgs
+                dmgs = random.randint(1,20)
+                dmgp = random.randint(1,20)
+                print('Joe mama got new pan')
+            z1 = 0
+            z2 = 1
+        elif z1 >= z2 and choice != 'f' and choice != 'h':
+            print ('Joe mama attacks')
+            hps = hps - dmgp
+            z1 = 0
+            z2 = 1
+            pktg +=1
+            if hps >0:
+                print(f'Joe dada still have {hps} hp')
+        elif z2 >= z1:
+            print ('Joe dada attacks')
+            hpp = hpp - dmgs
+            z1 = 1
+            z2 = 0
+            if hpp > 0:
+                print(f'Joe mama still has {hpp} hp')
+            time.sleep(0.1)
+            print('  h - heal (-1 point), f - fight')
+            print(f'You have {pktg} points')
+            choice = input('What do you want to do? ')
+        time.sleep(0.1)
+    if hpp > hps:
+        print(F'Joe mama wins!')
+        print(f'Joe mama  won the game with {pktg} points. GG!')
+    else:
+        print('Joe dada won')
+        print(f'If not Imposter\'s death he would win the game with {pktg} points')
 elif Gamer == 'amogus' or Gamer == 'Impostor' or Gamer == 'Imposter':
     dmgs = random.randint(1,20)
     dmgp = random.randint(1,20)
